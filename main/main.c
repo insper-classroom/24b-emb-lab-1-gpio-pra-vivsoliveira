aab465e3c6f49cd3b81fdd307116e9f93f1e01e7
vivsoliveira <victoriaof@al.insper.edu.br>
2024-08-14 19:15:19 -0300

    motor funciona

--- main/main.c
+++ main/main.c
@@ -6,8 +6,8 @@ const int LED_PIN_R = 2;
 const int LED_PIN_RO = 3;
 const int LED_PIN_B = 4;
 const int LED_PIN_Y = 5;
-
 const int BTN_PIN_R = 16;
+const int LED = 18;
 
 int main() {
   stdio_init_all();
@@ -27,31 +27,36 @@ int main() {
   gpio_init(LED_PIN_Y);
   gpio_set_dir(LED_PIN_Y, GPIO_OUT);
 
+  gpio_init(LED);
+  gpio_set_dir(LED, GPIO_OUT);
+  gpio_put(LED, 0);
+
   while (true) {
     if (!gpio_get(BTN_PIN_R)) {
-      // Debouncing antes de processar
-      sleep_ms(50);
+
+      gpio_put(LED, 1);
       for (int i = 0; i < 512; i++) {
         gpio_put(LED_PIN_R, 1);
-        sleep_ms(50);
+        sleep_ms(10);
         gpio_put(LED_PIN_R, 0);
 
         gpio_put(LED_PIN_RO, 1);
-        sleep_ms(50);
+        sleep_ms(10);
         gpio_put(LED_PIN_RO, 0);
 
         gpio_put(LED_PIN_B, 1);
-        sleep_ms(50);
+        sleep_ms(10);
         gpio_put(LED_PIN_B, 0);
 
         gpio_put(LED_PIN_Y, 1);
-        sleep_ms(50);
+        sleep_ms(10);
         gpio_put(LED_PIN_Y, 0);
       }
+      gpio_put(LED, 0);
       while (!gpio_get(BTN_PIN_R)) {
         // Mantém o loop até o botão ser liberado
       }
-      sleep_ms(50);
+      sleep_ms(10);
     }
   }
 }
